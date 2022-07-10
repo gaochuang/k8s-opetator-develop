d1d18093c1610f7747f9e7059e6888998608c3a4
gaochuang <ienjoyarmlinux@163.com>
2022-07-10 15:25:39 +0800

    Update

--- project/operator-crd/main.go
+++ project/operator-crd/main.go
@@ -1,11 +1,32 @@
 package main
 
-import "k8s.io/client-go/tools/clientcmd"
+import (
+	"context"
+	"fmt"
+	"log"
+	clientset "operator-crd/pkg/client/clientset/versioned"
+
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"k8s.io/client-go/tools/clientcmd"
+)
 
 func main() {
-	config, err := clientcmd.BuildConfigFromFlags("", clientcmd.RecommendedConfigPathFlag)
+	config, err := clientcmd.BuildConfigFromFlags("", clientcmd.RecommendedHomeFile)
 	if err != nil {
-		panic(err.Error())
+		log.Fatalln(err)
+	}
+	client, err := clientset.NewForConfig(config)
+	if err != nil {
+		log.Fatalln(err)
 	}
 
+	list, err := client.CrdV1().Foos("default").List(context.TODO(), v1.ListOptions{})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for _, foo := range list.Items {
+		fmt.Println(foo.Name)
+	}
 }
--- project/operator-crd/pkg/apis/crd.example.com/v1/register.go
+++ project/operator-crd/pkg/apis/crd.example.com/v1/register.go
@@ -9,7 +9,7 @@ import (
 )
 
 // SchemeGroupVersion is group version used to register these objects
-var SchemeGroupVersion = schema.GroupVersion{Group: "crd.example.com", Version: "v1alpha1"}
+var SchemeGroupVersion = schema.GroupVersion{Group: "crd.example.com", Version: "v1"}
 
 // Kind takes an unqualified kind and returns back a Group qualified GroupKind
 func Kind(kind string) schema.GroupKind {
